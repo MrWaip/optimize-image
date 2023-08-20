9e0734e62855da7b332b2b2949d7b4be8e176625
Konstantin Lobkov <klobkov@ozon.ru>
2023-08-20 15:11:41 +0300

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,36 +2,11 @@
 	import Picture from '$lib/Picture.svelte';
 	import Source from '../images/source1.png';
 	import Image from '../images/source1.png?optimized';
-	import JpegImg from '../images/big-jpeg.jpg?optimized';
-
+	import JPEG from '../images/big-jpeg.jpg?optimized';
 </script>
 
 <div class="container">
-	<div>
-		<Picture src={Source} />
-
-		<div>
-			Original
-		</div>
-	</div>
-
-	<div>
-		<Image /> 
-
-		
-		<div>
-			Optimized
-		</div>
-	</div>
-
-	<div>
-		<JpegImg /> 
-
-		
-		<div>
-			Jpeg Optimized
-		</div>
-	</div>
+	<JPEG />
 </div>
 
 <style>
